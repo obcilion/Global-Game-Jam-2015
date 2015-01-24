e572b95cb1102a78edc7813d538d37ba027f3850
Ole Edvard Berg Leren <oleedvardbergleren@gmail.com>
2015-01-24 15:09:25 +0100

    MoveAction coming together.

--- What Do We Do Now/Assets/Scripts/Actions/MoveAction.cs
+++ What Do We Do Now/Assets/Scripts/Actions/MoveAction.cs
@@ -4,8 +4,10 @@ using System;
 
 public class MoveAction : IAction
 {
-    private float _tweenDuration = 1f;
+    private float _tweenDuration = .5f;
     private float _moveDistance = 5f;
+
+    private TweenEasingCallback _easing = TweenEasingFunctions.GetFunction(EasingType.QuadEaseInOut);
 
     public void Perform(Action callback, GameObject targetShapee)
     {
@@ -21,6 +23,7 @@ public class MoveAction : IAction
                 targetShapee.transform.position.y,
                 targetShapee.transform.position.z))
             .SetDuration(_tweenDuration)
+            .SetEasing(_easing)
             .OnStopped(sender => callback())
             .Play();
 
