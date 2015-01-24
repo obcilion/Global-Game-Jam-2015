ab3ee9f17b80378136ecbf18a837e669ce064cfe
Raymond Gulbrandsen <raymond.gulbr@gmaill.com>
2015-01-24 22:36:10 +0100

    Wait Action Done

--- What Do We Do Now/Assets/Scripts/Actions/WaitAction.cs
+++ What Do We Do Now/Assets/Scripts/Actions/WaitAction.cs
@@ -3,20 +3,9 @@ using System;
 
 public class WaitAction : MonoBehaviour, IAction
 {
-    private Action _callback;
-    private bool _isMoving = false;
-    private int _direction = 1;
-
     public void Perform(Action callback)
     {
-        _callback = callback;
-
         Debug.Log("Performing Wait Action");
-
-    }
-
-    private void FixedUpdate()
-    {
-
+        callback();
     }
 }
