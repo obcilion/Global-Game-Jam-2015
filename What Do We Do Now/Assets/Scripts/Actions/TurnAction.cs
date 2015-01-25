6a30f8ef325db35ead0a4ad54e324b5d378a6894
yrojjory <me@jory.org>
2015-01-25 11:29:21 +0100

    Added audio triggers into actions

--- What Do We Do Now/Assets/Scripts/Actions/TurnAction.cs
+++ What Do We Do Now/Assets/Scripts/Actions/TurnAction.cs
@@ -6,6 +6,11 @@ public class TurnAction : MonoBehaviour, IAction
     public void Perform(Action callback)
     {
         Debug.Log("Performing Turn Action");
+
+		// play a sound
+		Debug.Log("Play a turn sound");
+        AudioManager.PlaySound("FX/Character/Turn",gameObject);
+
         gameObject.GetComponent<ShapeeBase>().Direction *= -1;
         callback();
     }
